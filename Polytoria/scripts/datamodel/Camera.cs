a0003b0f2aeca64127b8dd855d944310ed3821d4
arlocomotive <arloyamins@gmail.com>
2026-05-12 11:12:00 -0500

    init impl
    Signed-off-by: arlocomotive <arloyamins@gmail.com>

--- Polytoria/scripts/datamodel/Camera.cs
+++ Polytoria/scripts/datamodel/Camera.cs
@@ -20,6 +20,8 @@ public sealed partial class Camera : Dynamic
 	public const float ClipSafeMargin = 2.0f;
 	public const float DefaultZoomDistance = 10.0f;
 	public const float DefaultScrollSensitivity = 15.0f;
+	public const float GamepadSensitivity = 400.0f;
+
 	private CameraModeEnum _mode;
 	private float _fov;
 	private bool _clipThroughWalls;
@@ -421,7 +423,7 @@ public sealed partial class Camera : Dynamic
 				float xAxis = Input.GetAxis("cam_rightward", "cam_leftward");
 				float yAxis = Input.GetAxis("cam_downward", "cam_upward");
 
-				_targetRotation += new Vector3(yAxis, xAxis, 0) * Sensitivity;
+				_targetRotation += new Vector3(yAxis * 0.75f, xAxis, 0) * (Sensitivity * GamepadSensitivity * (float)delta);
 				LimitRotation();
 			}
 
