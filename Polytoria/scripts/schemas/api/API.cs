170ddb4091b9cef9c3c139e66022b6ce68c4ab35
Full Moon <cp2192437@gmail.com>
2026-06-01 17:09:35 +0300

    a
    Signed-off-by: Full Moon <cp2192437@gmail.com>

--- Polytoria/scripts/schemas/api/API.cs
+++ Polytoria/scripts/schemas/api/API.cs
@@ -1,6 +1,9 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
+
 // License, v. 2.0. If a copy of the MPL was not distributed with this
+
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 
 using System;
 using System.Text.Json.Serialization;
@@ -322,7 +325,6 @@ public struct APIGuildInfo
 
 	[JsonPropertyName("createdAt")]
 	public DateTime CreatedAt { get; set; }
-	
 }
 
 public struct APIMeResponse
