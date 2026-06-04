70ee7f6e4fcd5fdfcc7280a43aaf9c8c4b5a678c
white-moon-code <cp2192437@gmail.com>
2026-06-04 23:02:45 +0300

    fix positioning of chat bubble when a part is parented to the player
    Signed-off-by: white-moon-code <cp2192437@gmail.com>

--- Polytoria/scripts/client/spatial/chat/BubbleChat.cs
+++ Polytoria/scripts/client/spatial/chat/BubbleChat.cs
@@ -13,7 +13,7 @@ public partial class BubbleChat : Node3D
 {
 	public string BubbleItemPath = "res://scenes/client/spatial/chat/bubble_item.tscn";
 	private const int BubbleCountLimit = 5;
-	public const float BubbleHeightMinus = 2f;
+	public const float BubbleHeightPlus = 4f;
 	private readonly List<BubbleItem> _activeBubbles = [];
 
 	[Export] private Control _itemContainer = null!;
@@ -33,16 +33,16 @@ public partial class BubbleChat : Node3D
 
 	private void OnPlayerChatted(string msg)
 	{
-		Aabb? bounds = TargetPlayer.CalculateBounds();
+		Aabb? bounds = TargetPlayer.Character.GetAttachment(CharacterModel.CharacterAttachmentEnum.Head).CalculateBounds();
 
 		if (bounds.HasValue)
 		{
-			int additional = 0;
+			int decrease = 0;
 			if (TargetPlayer.IsLocal)
 			{
-				additional = 1;
+				decrease = 1;
 			}
-			Position = new Vector3(0, bounds.Value.Size.Y - BubbleHeightMinus - additional, 0);
+			Position = new Vector3(0, bounds.Value.Size.Y + BubbleHeightPlus - decrease, 0);
 		}
 
 		BubbleItem item = Globals.CreateInstanceFromScene<BubbleItem>(BubbleItemPath);
