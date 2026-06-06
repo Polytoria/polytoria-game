4691d88386c27a13dc9847348619cb0993a8fb5f
white-moon-code <cp2192437@gmail.com>
2026-06-06 20:14:16 +0300

    a
    Signed-off-by: white-moon-code <cp2192437@gmail.com>

--- Polytoria/scripts/datamodel/Seat.cs
+++ Polytoria/scripts/datamodel/Seat.cs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using System.Numerics;
-using System.Runtime.Intrinsics.X86;
 using Polytoria.Attributes;
 using Polytoria.Scripting;
 
