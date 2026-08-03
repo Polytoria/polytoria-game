cee03a359217f5d6465f8f11f0b2bf2969576f06
JustBorgar <134341430+JustBorgar@users.noreply.github.com>
2026-08-03 20:41:23 -0300

    Adds singleton property to modulescripts
    Signed-off-by: JustBorgar <134341430+JustBorgar@users.noreply.github.com>

--- Polytoria/scripts/datamodel/ModuleScript.cs
+++ Polytoria/scripts/datamodel/ModuleScript.cs
@@ -3,6 +3,7 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 using Polytoria.Attributes;
+using Polytoria.Scripting.Luau;
 
 namespace Polytoria.Datamodel;
 
@@ -11,10 +12,31 @@ public sealed partial class ModuleScript : Script
 {
 	internal int? CachedLuauResultRef { get; set; } = null;
 
+	private bool _singleton = false;
+	private bool _singletonRequired = false;
+
+	[Editable, ScriptProperty]
+	public bool Singleton
+	{
+		get => _singleton;
+		set
+		{
+			_singleton = value;
+			OnPropertyChanged();
+			TryRunSingleton();
+		}
+	}
+
 	public override void EnterTree()
 	{
 		CheckSource();
 		base.EnterTree();
+	}
+
+	public override void Ready()
+	{
+		TryRunSingleton();
+		base.Ready();
 	}
 
 	internal void CheckSource()
@@ -28,6 +50,28 @@ public sealed partial class ModuleScript : Script
 		}
 	}
 
+	internal void NotifyBytecodeReceived()
+	{
+		TryRunSingleton();
+	}
+
+	private void TryRunSingleton()
+	{
+		if (!_singleton || _singletonRequired) return;
+		if (Root == null || IsDeleted) return;
+		if (Root.SessionType == World.SessionTypeEnum.Creator) return;
+		if (!IsEnabled || IsHidden) return;
+		if (!IsNetworkReady) return;
+
+		if (!Root.IsLoaded)
+		{
+			Root.Loaded.Once(TryRunSingleton);
+			return;
+		}
+
+		_singletonRequired = true;
+		LuauProvider.Singleton.RequireSingleton(this);
+	}
 
 	private void RequestSource()
 	{
