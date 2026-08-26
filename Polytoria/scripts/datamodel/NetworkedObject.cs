4a97b09d1bf6fc8d890f52c1b4e1eb933f78f76f
bluefield <bluefield@noreply.open-sbc.org>
2026-08-26 05:03:33 +0200

    Perf(Datamodel): reference equality for NetworkedObject
    Signed-off-by: bluefield <bluefield@noreply.open-sbc.org>

--- Polytoria/scripts/datamodel/NetworkedObject.cs
+++ Polytoria/scripts/datamodel/NetworkedObject.cs
@@ -1987,7 +1987,12 @@ public partial class NetworkedObject : IScriptObject
 	}
 
 	[ScriptMetamethod(ScriptObjectMetamethod.Eq)]
-	public static bool MetamethodEquals(object? a, object? b) => a is NetworkedObject netobj && netobj.Equals(b);
+	public static bool MetamethodEquals(object? a, object? b) =>
+		a is NetworkedObject na &&
+		(ReferenceEquals(na, b) ||
+		(b is NetworkedObject nb &&
+		na.ExistInNetwork == nb.ExistInNetwork &&
+		(na.ExistInNetwork ? na.NetworkedObjectID == nb.NetworkedObjectID : na.ObjectID == nb.ObjectID)));
 
 	internal IEnumerable<PropertyInfo> GetEditableProperties()
 	{
@@ -2020,16 +2025,6 @@ public partial class NetworkedObject : IScriptObject
 			)]
 		);
 #pragma warning restore IL2070 // 'this' argument does not satisfy 'DynamicallyAccessedMembersAttribute' in call to target method. The parameter of method does not have matching annotations.
-	}
-
-	public override bool Equals(object? obj) =>
-		obj is NetworkedObject netobj &&
-		ExistInNetwork == netobj.ExistInNetwork &&
-		(ExistInNetwork ? NetworkedObjectID == netobj.NetworkedObjectID : ObjectID == netobj.ObjectID);
-
-	public override int GetHashCode()
-	{
-		return base.GetHashCode();
 	}
 
 #if DEBUG
