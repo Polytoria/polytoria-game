d96b6abdda7931e774ee0dcdf52e4666179c9d09
benja2998 <benja2998@proton.me>
2026-08-27 17:28:48 -0400

    make infinitely faster

--- Polytoria/scripts/shared/settings/SettingSectionDef.cs
+++
@@ -1,9 +0,0 @@
-namespace Polytoria.Shared.Settings;
-
-public class SettingSectionDef
-{
-	public required string Key { get; init; }
-	public required string Label { get; init; }
-	public string IconPath { get; init; } = string.Empty;
-	public int SortOrder { get; init; } = 0;
-}
--- addlicense.ps1
+++
@@ -1,9 +0,0 @@
-# Requires https://github.com/google/addlicense
-
-Get-ChildItem -Recurse -Filter *.cs |
-Where-Object {
-    $_.FullName -notlike "*Polytoria\addons\*"
-} |
-ForEach-Object {
-    addlicense -c "Polytoria" -l "MPL-2.0" $_.FullName
-}
