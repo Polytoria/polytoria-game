3fa0019c0bfe4bd63c3f9ee0701f993068863a81
Diamond <61511674+DiamondRoPlayz@users.noreply.github.com>
2026-08-31 23:07:27 +0100

    Update MaxSize to use dynamic MaxPlayers value

--- Polytoria/scripts/datamodel/services/PresenceService.cs
+++ Polytoria/scripts/datamodel/services/PresenceService.cs
@@ -218,7 +218,7 @@ public sealed partial class PresenceService : Instance
 				Size =
 				{
 					CurrentSize = Root.Players.PlayersCount,
-					MaxSize = 20,
+					MaxSize = Root.WorldInfo?.MaxPlayers ?? 8
 				},
 			},
 			Instance = true
