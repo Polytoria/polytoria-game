a50037b9f715376f82b39d255f822e42e008a018
arlocomotive <arloyamins@gmail.com>
2026-08-31 21:21:33 -0500

    oops
    Signed-off-by: arlocomotive <arloyamins@gmail.com>

--- Polytoria/scripts/scripting/languages/luau/LuaMetatable.cs
+++ Polytoria/scripts/scripting/languages/luau/LuaMetatable.cs
@@ -296,9 +296,9 @@ public class LuaMetatable : LuaObject
 				throw new UnauthorizedAccessException("script does not have permission to access the specified property (" + key + ")");
 			}
 
-			if (accessData.ObsoleteAttribute != null)
+			if (accessData.ObsoletionInfo.HasValue)
 			{
-				PT.PrintWarn(accessData.ObsoleteAttribute.GetWarning(prop.Name));
+				PT.PrintWarn(accessData.ObsoletionInfo.Value.GetWarning(prop.Name));
 			}
 
 			object? value = script.Compatibility
@@ -864,9 +864,9 @@ public class LuaMetatable : LuaObject
 			throw new UnauthorizedAccessException("script does not have permission to call the specified method (" + targetMethod.Name + ")");
 		}
 
-		if (invocationData.ObsoleteAttribute != null)
+		if (invocationData.ObsoletionInfo.HasValue)
 		{
-			PT.PrintWarn(invocationData.ObsoleteAttribute.GetWarning(targetMethod.Name));
+			PT.PrintWarn(invocationData.ObsoletionInfo.Value.GetWarning(targetMethod.Name));
 		}
 
 		// Prepare args array formatted for params
@@ -943,7 +943,7 @@ public class LuaMetatable : LuaObject
 		Attributes.ObsoleteAttribute? obsoleteAttribute = property.GetCustomAttribute<Attributes.ObsoleteAttribute>();
 		PropertyAccessData result = new(
 			scriptAttribute?.Permissions ?? ScriptPermissionFlags.None,
-			obsoleteAttribute,
+			obsoleteAttribute?.Info,
 			ScriptInterfaceInvokers.TryGetGetter(property),
 			ScriptInterfaceInvokers.TryGetSetter(property));
 		_propertyAccessCache[property] = result;
@@ -962,7 +962,7 @@ public class LuaMetatable : LuaObject
 			parameters,
 			Array.FindIndex(parameters, p => p.IsDefined(typeof(ScriptingCallerAttribute))),
 			scriptAttribute?.Permissions ?? ScriptPermissionFlags.None,
-			obsoleteAttribute,
+			obsoleteAttribute?.Info,
 			ScriptService.IsAsyncMethod(method),
 			ScriptInterfaceInvokers.TryGetInvoker(method));
 		_methodInvocationCache[method] = result;
@@ -1101,7 +1101,7 @@ public class LuaMetatable : LuaObject
 
 	private readonly record struct PropertyAccessData(
 		ScriptPermissionFlags Permissions,
-		Attributes.ObsoleteAttribute? ObsoleteAttribute,
+		ObsoletionInfo? ObsoletionInfo,
 		Func<object, object?>? Getter,
 		Action<object, object?>? Setter);
 
@@ -1109,7 +1109,7 @@ public class LuaMetatable : LuaObject
 		ParameterInfo[] Parameters,
 		int CallerParamIndex,
 		ScriptPermissionFlags Permissions,
-		Attributes.ObsoleteAttribute? ObsoleteAttribute,
+		ObsoletionInfo? ObsoletionInfo,
 		bool IsAsync,
 		Func<object?, object?[], object?>? Invoker);
 }
