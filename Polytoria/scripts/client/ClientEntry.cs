83943f442d6ede8f3e3d6dc7bc773f43824b108b
JustBorgar <134341430+JustBorgar@users.noreply.github.com>
2026-09-15 04:03:11 -0300

    Removed leftover line
    Signed-off-by: JustBorgar <134341430+JustBorgar@users.noreply.github.com>

--- Polytoria/scripts/client/ClientEntry.cs
+++ Polytoria/scripts/client/ClientEntry.cs
@@ -557,8 +557,6 @@ public sealed partial class ClientEntry : Node3D
 			args.AddRange(["-debug", $"{_debugAddress}:{_debugPort.Value}"]);
 		}
 
-		args.AddRange("--rendering-method", RenderingDeviceSwitcher.GetCurrentDriverName());
-
 		// Boot into client's saved renderer
 		string? clientRenderingMethod = CreatorService.GetStoredClientRenderingMethodArg();
 		if (clientRenderingMethod != null)
